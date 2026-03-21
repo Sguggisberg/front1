edd4b3d8af7695f846d364027b411b37017bcd88
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2026-03-21 09:32:13 +0100

    setup try

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  name: 'prod',
-  apiPath: '/prod/api'
-}
+  production: true
+};
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: false,
-  name: 'dev',
-  apiPath: '/api'
-}
+};
