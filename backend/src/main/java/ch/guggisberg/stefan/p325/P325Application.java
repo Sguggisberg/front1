8a94b515da579eba378976dd1b37639c77ddc77b
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2026-03-30 11:40:35 +0200

    setup test

---
+++ backend/src/main/java/ch/guggisberg/stefan/p325/P325Application.java
@@ -0,0 +1,13 @@
+package ch.guggisberg.stefan.p325;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P325Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/guggisberg/stefan/p325/P325ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.guggisberg.stefan.p325;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P325ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
