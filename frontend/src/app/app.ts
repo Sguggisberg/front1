b17fbf01340259f415b1525ba7bd7657939adbc1
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2026-03-30 12:24:50 +0200

    setup test

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,19 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { JsonPipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe, JsonPipe],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   private http = inject(HttpClient);
 
-  test = this.http.get<string>('http://localhost/api/users');
+  test = this.http.get<string>('http://178.128.192.124/api/users');
   counter = toSignal(this.test);
 
   protected readonly title = signal('front');
