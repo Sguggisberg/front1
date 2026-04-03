e04c728d0c31d1a0cd7748d82bca39b7a1443a6f
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2026-04-03 10:29:58 +0200

    setup test

--- backend/src/main/java/ch/guggisberg/stefan/p325/controller/Test.java
+++ backend/src/main/java/ch/guggisberg/stefan/p325/controller/Test.java
@@ -1,22 +1,24 @@
 package ch.guggisberg.stefan.p325.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://178.128.192.124", "http://localhost"})
 @RestController
 @RequestMapping("/api/users")
 public class Test {
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     @GetMapping
     public ResponseEntity<Hallo> test() {
 
         Hallo hello = new Hallo();
-        hello.text="hello world7";
+        hello.text=allowedOrigins;
 
         return new ResponseEntity<>(hello, HttpStatus.OK);
     }
